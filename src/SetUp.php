c8b029e46248f0d9f135f6531eb1a566b447bf96
Bradford Condon <bradford.condon@gmail.com>
2018-03-20 14:20:57 -0400

    run beautify

--- src/SetUp.php
+++ src/SetUp.php
@@ -13,58 +13,60 @@ class SetUp
    *
    * @var string
    */
-  protected $root;
+    protected $root;
 
-  protected $vendor_root;
-
-
-public function run(){
-$root =  getcwd();
-$this->root = $root;
-$this->vendor_root = $root;
-$this->setUpTests();
-$this->addTravis();
-}
+    protected $vendor_root;
 
 
-protected function setUpTests(){
-    $root = $this->root;
-    $vendor_root = $this->vendor_root;
+    public function run()
+    {
+        $root =  getcwd();
+        $this->root = $root;
+        $this->vendor_root = $root;
+        $this->setUpTests();
+        $this->addTravis();
+    }
 
-  $this->_create_dir("tests");
-  copy($vendor_root . "/stubs/TripalExampleTest.php.test", $root . "/tests/TripalExampleTest.php.test");
 
-  copy($vendor_root . "/stubs/example.env", $root . "/tests/example.env");
+    protected function setUpTests()
+    {
+        $root = $this->root;
+        $vendor_root = $this->vendor_root;
 
-  if (file_exists($root . "/phpunit.xml")){
-    print  "\nphpunit.xml already exists: skipping...\n";
-  }else {
-    copy($vendor_root . "/stubs/phpunit.xml", $root . "/phpunit.xml");
-  }
-}
+        $this->_create_dir("tests");
+        copy($vendor_root . "/stubs/TripalExampleTest.php.test", $root . "/tests/TripalExampleTest.php.test");
 
-protected function _create_dir($dir){
-  $root = $this->root;
-  $dir = "/" . $dir;
-  if (!file_exists($root . $dir)){
-    if (!mkdir($root . $dir)) {
-      throw new Exception(
+        copy($vendor_root . "/stubs/example.env", $root . "/tests/example.env");
+
+        if (file_exists($root . "/phpunit.xml")) {
+            print  "\nphpunit.xml already exists: skipping...\n";
+        } else {
+            copy($vendor_root . "/stubs/phpunit.xml", $root . "/phpunit.xml");
+        }
+    }
+
+    protected function _create_dir($dir)
+    {
+        $root = $this->root;
+        $dir = "/" . $dir;
+        if (!file_exists($root . $dir)) {
+            if (!mkdir($root . $dir)) {
+                throw new Exception(
         "Could not create" . $root . $dir . " !\n"
 );
-    };
-  }
-}
+            };
+        }
+    }
 
-protected function addTravis(){
+    protected function addTravis()
+    {
+        $root = $this->root;
+        $vendor_root = $this->vendor_root;
 
-  $root = $this->root;
-  $vendor_root = $this->vendor_root;
-
-  if (file_exists($root . "/.travis.yml")){
-    print  "\n.travis.yml file already exists: skipping Travis Integration step...\n";
-  }else {
-  copy($vendor_root . "/stubs/travis.yml", $root . "/.travis.yml");
-}
-
-}
+        if (file_exists($root . "/.travis.yml")) {
+            print  "\n.travis.yml file already exists: skipping Travis Integration step...\n";
+        } else {
+            copy($vendor_root . "/stubs/travis.yml", $root . "/.travis.yml");
+        }
+    }
 }
