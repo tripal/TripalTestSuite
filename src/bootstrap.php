d33e9e9780fe4e95e0575b83d3e09e64b1f158a3
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-03-22 11:49:24 -0400

    make sure load_php_files is only declared once

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
 load_php_files('Console');
 
-function load_php_files($dir) {
-    $dir = __DIR__.'/'.$dir;
+if (! function_exists('load_php_files')) {
+    function load_php_files($dir)
+    {
+        $dir = __DIR__.'/'.$dir;
 
-    foreach (glob($dir.'/*.php') as $file) {
-        require_once $file;
+        foreach (glob($dir.'/*.php') as $file) {
+            require_once $file;
+        }
     }
 }
--- tests/test_module/tests/bootstrap.php
+++ tests/test_module/tests/bootstrap.php
@@ -3,10 +3,13 @@ load_php_files('TripalTestSuite');
 load_php_files('TripalTestSuite/Exceptions');
 load_php_files('TripalTestSuite/Mocks');
 
-function load_php_files($dir) {
-    $dir = __DIR__.'/'.$dir;
+if (! function_exists('load_php_files')) {
+    function load_php_files($dir)
+    {
+        $dir = __DIR__.'/'.$dir;
 
-    foreach (glob($dir.'/*.php') as $file) {
-        require_once $file;
+        foreach (glob($dir.'/*.php') as $file) {
+            require_once $file;
+        }
     }
 }
