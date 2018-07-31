32061a2210d3374ca4c061d37fc60739204164de
Bradford Condon <bradford.condon@gmail.com>
2018-07-31 11:13:15 -0400

    formatting

--- stubs/DevseedSeeder.php
+++ stubs/DevseedSeeder.php
@@ -6,16 +6,15 @@ use StatonLab\TripalTestSuite\Database\Seeder;
 
 class DevSeedSeeder extends Seeder {
 
-
-/**
-* Part one:
-* Chado records.
-* These are records used by many of the below importers.
-* ALL importers require an organism.
-* The expression data loader will associate the data with the $expression_analysis record.
-* All other importers associate data with the $sequence_analysis.
-* Uncomment the array to create that chado record.
-*/
+  /**
+   * Part one:
+   * Chado records.
+   * These are records used by many of the below importers.
+   * ALL importers require an organism.
+   * The expression data loader will associate the data with the
+   * $expression_analysis record. All other importers associate data with the
+   * $sequence_analysis. Uncomment the array to create that chado record.
+   */
 
 
 // protected $organism = [
@@ -36,14 +35,14 @@ class DevSeedSeeder extends Seeder {
 //       'name' => 'Fraxinus exclesior miniature dataset Expression Analysis',
 //       'description' => 'Tripal Dev Seed',
 //     ];
-  
+
 //   protected $blastdb = [
 //     'name' => 'DevSeed Database: TREMBL',
 //     'description' => 'A dummy database created by DevSeed',
 //   ];
 
   /**
-  * Part 2:
+   * Part 2:
    * Files.
    * Each importer will take a file argument.  This argument should be an array
    * with one of the following two keys: file_remote => url where the file is
@@ -51,15 +50,13 @@ class DevSeedSeeder extends Seeder {
    */
 
 
-
-
 //  protected $landmark_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/empty_landmarks.fasta'];
 
   protected $landmark_type = 'scaffold';
 
- // protected $mRNA_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/mrna_mini.fasta'];
+  // protected $mRNA_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/mrna_mini.fasta'];
 
- // protected $protein_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/polypeptide_mini.fasta'];
+  // protected $protein_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/polypeptide_mini.fasta'];
 
   //protected $gff_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/gff/filtered.gff'];
 
@@ -79,207 +76,236 @@ class DevSeedSeeder extends Seeder {
 
   public function __construct() {
 
-if ($this->organism){
-
-  try {
-
- $organism = this->fetch_chado_record('chado.organism', ['common_name', 'organism_id'], $this->organism);
-
-} catch (\Exception $e) {
-echo $e->getMessage();
-exit;
-}
-
-$this->organism = $organism;
-
-    if ($this->sequence_analysis){
+    if ($this->organism) {
 
       try {
+        $organism = $this->fetch_chado_record('chado.organism', [
+          'common_name',
+          'organism_id',
+        ], $this->organism);
 
- $seq_analysis = this->fetch_chado_record('chado.analysis', ['analysis_id'], $this->sequence_analysis);
+      }
+      catch (\Exception $e) {
+        echo $e->getMessage();
+        exit;
+      }
 
-} catch (\Exception $e) {
-echo $e->getMessage();
-exit;
-}  
-    $this->sequence_analysis = $seq_analysis;
+      $this->organism = $organism;
 
-$expression_analysis = this->fetch_chado_record('chado.analysis', ['analysis_id'], $this->expression_analysis);
+      if ($this->sequence_analysis) {
 
-} catch (\Exception $e) {
-echo $e->getMessage();
-exit;
-}
+        try {
+          $seq_analysis = $this->fetch_chado_record('chado.analysis', ['analysis_id'], $this->sequence_analysis);
 
-    $this->expression_analysis = $expression_analysis;
+        }
+        catch (\Exception $e) {
+          echo $e->getMessage();
+          exit;
+        }
+        $this->sequence_analysis = $seq_analysis;
+      }
 
-    if ($this->blastdb){
+      if ($this->expression_analysis) {
+        try {
+          $expression_analysis = $this->fetch_chado_record('chado.analysis', ['analysis_id'], $this->expression_analysis);
+        }
+        catch
+        (\Exception $e) {
+          echo $e->getMessage();
+          exit;
+        }
 
-try {
-      $blastdb = $this->fetch_chado_record('chado.db', ['db_id'], $this->blastdb);
+        $this->expression_analysis = $expression_analysis;
+      }
     }
-    catch(\Excetion $e){
-      echo $e->getMessage();
+
+    if ($this->blastdb) {
+      try {
+        $blastdb = $this->fetch_chado_record('chado.db', ['db_id'], $this->blastdb);
+      }
+      catch (\Excetion $e) {
+        echo $e->getMessage();
+      }
+
+      $this->blastdb = $blastdb;
+
     }
-
-$this->blastdb =  $blastdb;
-
   }
 
   /**
-   * Uncomment loaders you would like to run.
+   * Runs all loaders.
+   * Will only run loaders where the files have been uncommented at the start
+   * of the class.
    */
   public function up() {
 
-    //first load landmarks.
+    if ($this->landmark_file) {
 
-
-    $run_args = [
-      'organism_id' => $this->organism->organism_id,
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      'seqtype' => $this->landmark_type,
-      'method' => 2, //default insert and update
-      'match_type' => 1, //unique name default
-      //optional
-      're_name' => NULL,
-      're_uname' => NULL,
-      're_accession' => NULL,
-      'db_id' => NULL,
-      'rel_type' => NULL,
-      're_subject' => NULL,
-      'parent_type' => NULL,
-
-    ];
-    
-if ($this->landmark_file){
+      $run_args = [
+        'organism_id' => $this->organism->organism_id,
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        'seqtype' => $this->landmark_type,
+        'method' => 2, //default insert and update
+        'match_type' => 1, //unique name default
+        //optional
+        're_name' => NULL,
+        're_uname' => NULL,
+        're_accession' => NULL,
+        'db_id' => NULL,
+        'rel_type' => NULL,
+        're_subject' => NULL,
+        'parent_type' => NULL,
+      ];
       $this->load_landmarks($run_args, $this->landmark_file);
-
-}
-
-
-    $run_args = [
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      'organism_id' => $this->organism->organism_id,
-
-      'use_transaction' => 1,
-      'add_only' => 0,
-      'update' => 1,
-      'create_organism' => 0,
-      'create_target' => 0,
-
-      ///regexps for mRNA and protein.
-      're_mrna' => NULL,
-      're_protein' => $this->prot_regexp,
-      //optional
-      'target_organism_id' => NULL,
-      'target_type' => NULL,
-      'start_line' => NULL,
-      'landmark_type' => NULL,
-      'alt_id_attr' => NULL,
-
-    ];
-
-    //  $this->load_GFF($run_args, $this->gff_file);
-
-
-    $run_args = [
-      'organism_id' => $this->organism->organism_id,
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      'seqtype' => 'mRNA',
-      'method' => 2, //default insert and update
-      'match_type' => 1, //unique name default
-      //optional
-      're_name' => NULL,
-      're_uname' => NULL,
-      're_accession' => NULL,
-      'db_id' => NULL,
-      'rel_type' => NULL,
-      're_subject' => NULL,
-      'parent_type' => NULL,
-
-    ];
-
-    $this->load_mRNA_FASTA($run_args, $this->mRNA_file);
-
-    $run_args = [
-      'organism_id' => $this->organism->organism_id,
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      'seqtype' => 'polypeptide',
-      'method' => 2,
-      'match_type' => 1,
-
-
-      //optional
-      're_name' => NULL,
-      're_uname' => NULL,
-      're_accession' => NULL,
-      'db_id' => NULL,
-
-    ];
-
-    if ($this->prot_regexp) {
-      //links polypeptide to mRNA
-      $run_args['rel_type'] = 'derives_from';
-      $run_args['re_subject'] = $this->prot_regexp;
-      $run_args['parent_type'] = 'mRNA';
     }
 
 
-     $this->load_polypeptide_FASTA($run_args, $this->protein_file);
+    if ($this->gff_file) {
+      $run_args = [
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        'organism_id' => $this->organism->organism_id,
+        'use_transaction' => 1,
+        'add_only' => 0,
+        'update' => 1,
+        'create_organism' => 0,
+        'create_target' => 0,
 
-    $run_args = [
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      //optional
-      'query_type' => mRNA,
-      'query_re' => $this->prot_regexp,
-      'query_uniquename' => NULL,
-      'parsego' => TRUE,
-    ];
+        ///regexps for mRNA and protein.
+        're_mrna' => NULL,
+        're_protein' => $this->prot_regexp,
+        //optional
+        'target_organism_id' => NULL,
+        'target_type' => NULL,
+        'start_line' => NULL,
+        'landmark_type' => NULL,
+        'alt_id_attr' => NULL,
+      ];
+      $this->load_GFF($run_args, $this->gff_file);
+    }
 
-    $this->load_interpro_annotations($run_args, $this->interpro_file);
+    if ($this->mRNA_file) {
 
-    $run_args = [
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      'no_parsed' => 25,//number results to parse
-      'query_type' => 'mRNA',
-      //optional
-      'blastdb' => $this->blastdb->db_id,
-      'blastfile_ext' => NULL,
-      'is_concat' => 0,
-      'query_re' => NULL,
-      'query_uniquename' => 0,
-    ];
+      $run_args = [
+        'organism_id' => $this->organism->organism_id,
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        'seqtype' => 'mRNA',
+        'method' => 2, //default insert and update
+        'match_type' => 1, //unique name default
+        //optional
+        're_name' => NULL,
+        're_uname' => NULL,
+        're_accession' => NULL,
+        'db_id' => NULL,
+        'rel_type' => NULL,
+        're_subject' => NULL,
+        'parent_type' => NULL,
+      ];
+      $this->load_mRNA_FASTA($run_args, $this->mRNA_file);
+    }
 
-    // $this->load_blast_annotations($run_args, $this->blast_file);
+    if ($this->protein_file) {
+      $run_args = [
+        'organism_id' => $this->organism->organism_id,
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        'seqtype' => 'polypeptide',
+        'method' => 2,
+        'match_type' => 1,
+        //optional
+        're_name' => NULL,
+        're_uname' => NULL,
+        're_accession' => NULL,
+        'db_id' => NULL,
+      ];
 
-    $run_args = [
-      'organism_id' => $this->organism->organism_id,
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-    ];
-    //optional: specifies specific CVterms for properties/property values.  Not used here.
-    //'cvterm_configuration' => NULL,
-    //'cvalue_configuration' => NULL];
+      if ($this->prot_regexp) {
+        //links polypeptide to mRNA
+        $run_args['rel_type'] = 'derives_from';
+        $run_args['re_subject'] = $this->prot_regexp;
+        $run_args['parent_type'] = 'mRNA';
+      }
+      $this->load_polypeptide_FASTA($run_args, $this->protein_file);
+    }
 
-    //	$this->load_biomaterials($run_args, $this->biomaterial_file);
+    if ($this->interpro_file) {
 
+      $run_args = [
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        //optional
+        'query_type' => mRNA,
+        'query_re' => $this->prot_regexp,
+        'query_uniquename' => NULL,
+        'parsego' => TRUE,
+      ];
 
-    $run_args = [
-      'filetype' => 'mat', //matrix file type
-      'organism_id' => $this->organism->organism_id,
-      'analysis_id' => $this->sequence_analysis->analysis_id,
-      //optional
-      'fileext' => NULL,
-      'feature_uniquenames' => 'uniq',
-      're_start' => NULL,
-      're_stop' => NULL,
-      'feature_uniquenames' => NULL,
-      'quantificationunits' => NULL,
-    ];
+      $this->load_interpro_annotations($run_args, $this->interpro_file);
+    }
 
-    //$this->load_expression($run_args, $this->expression_file);
+    if ($this->blast_file) {
+      $run_args = [
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        'no_parsed' => 25,//number results to parse
+        'query_type' => 'mRNA',
+        //optional
+        'blastdb' => $this->blastdb->db_id,
+        'blastfile_ext' => NULL,
+        'is_concat' => 0,
+        'query_re' => NULL,
+        'query_uniquename' => 0,
+      ];
+
+      $this->load_blast_annotations($run_args, $this->blast_file);
+    }
+
+    if ($this->biomaterial_file) {
+      $run_args = [
+        'organism_id' => $this->organism->organism_id,
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+      ];
+      //optional: specifies specific CVterms for properties/property values.  Not used here.
+      //'cvterm_configuration' => NULL,
+      //'cvalue_configuration' => NULL];
+
+      $this->load_biomaterials($run_args, $this->biomaterial_file);
+
+    }
+
+    if ($this->expression_file) {
+      $run_args = [
+        'filetype' => 'mat', //matrix file type
+        'organism_id' => $this->organism->organism_id,
+        'analysis_id' => $this->sequence_analysis->analysis_id,
+        //optional
+        'fileext' => NULL,
+        'feature_uniquenames' => 'uniq',
+        're_start' => NULL,
+        're_stop' => NULL,
+        'feature_uniquenames' => NULL,
+        'quantificationunits' => NULL,
+      ];
+      $this->load_expression($run_args, $this->expression_file);
+    }
   }
 
-  private function load_landmarks($run_args, $file) {
+  private
+  function load_landmarks($run_args, $file) {
+    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
+    $importer = new \FASTAImporter();
+    $importer->create($run_args, $file);
+    $importer->prepareFiles();
+    $importer->run();
+  }
+
+  private
+  function load_mRNA_FASTA($run_args, $file) {
+    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
+    $importer = new \FASTAImporter();
+    $importer->create($run_args, $file);
+    $importer->prepareFiles();
+    $importer->run();
+  }
+
+  private
+  function load_polypeptide_FASTA($run_args, $file) {
     module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
 
     $importer = new \FASTAImporter();
@@ -288,24 +314,8 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function load_mRNA_FASTA($run_args, $file) {
-    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
-    $importer = new \FASTAImporter();
-    $importer->create($run_args, $file);
-    $importer->prepareFiles();
-    $importer->run();
-  }
-
-  private function load_polypeptide_FASTA($run_args, $file) {
-    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
-
-    $importer = new \FASTAImporter();
-    $importer->create($run_args, $file);
-    $importer->prepareFiles();
-    $importer->run();
-  }
-
-  private function load_interpro_annotations($run_args, $file) {
+  private
+  function load_interpro_annotations($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_interpro', 'includes/TripalImporter/InterProImporter');
 
     $importer = new \InterProImporter();
@@ -314,7 +324,8 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function load_GFF($run_args, $file) {
+  private
+  function load_GFF($run_args, $file) {
     module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/GFF3Importer');
     $importer = new \GFF3Importer();
     $importer->create($run_args, $file);
@@ -322,7 +333,8 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function load_blast_annotations($run_args, $file) {
+  private
+  function load_blast_annotations($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_blast', 'includes/TripalImporter/BlastImporter');
     $importer = new \BlastImporter();
     $importer->create($run_args, $file);
@@ -330,7 +342,8 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function load_biomaterials($run_args, $file) {
+  private
+  function load_biomaterials($run_args, $file) {
     module_load_include('inc', 'tripal_biomaterial', 'includes/TripalImporter/tripal_biomaterial_loader_v3');
     $importer = new \tripal_biomaterial_loader_v3();
     $importer->create($run_args, $file);
@@ -338,7 +351,8 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function load_expression($run_args, $file) {
+  private
+  function load_expression($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_expression', 'includes/TripalImporter/tripal_expression_data_loader');
     $importer = new \tripal_expression_data_loader();
     $importer->create($run_args, $file);
@@ -346,22 +360,23 @@ if ($this->landmark_file){
     $importer->run();
   }
 
-  private function fetch_chado_record($table, $fields, $factory_array){
+  private
+  function fetch_chado_record($table, $fields, $factory_array) {
     $query = db_select($table, 't')
-    ->fields('t', $fields);
+      ->fields('t', $fields);
 
-    foreach ($factory_array as $key => $value ){
+    foreach ($factory_array as $key => $value) {
       $query->condition($key, $value);
     }
 
     $count_query = $query;
     $count_query->countQuery()->execute()->fetchField();
 
-    if ($count_query === 0){
+    if ($count_query === 0) {
       return factory($table)->create($factory_array);
     }
-    if ($count_query === 1){
-    return $query->execute()->fetchObject();
+    if ($count_query === 1) {
+      return $query->execute()->fetchObject();
     }
 
     throw new Exception("Error creating object for: " . $table
